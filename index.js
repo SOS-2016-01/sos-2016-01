6a4dcf2165efcaa4a206432166562785a9f51855
javyxz <javyxz@hotmail.com>
2016-02-29 19:19:07 +0100

    next version 4

--- index.js
+++ index.js
@@ -2,17 +2,24 @@ var express = require("express");
 
 var app = express();
 
-app.get("/about", (req, res) => {
+app.get("/", (req, res) => {
 
-  res.write("Tematica: co2, diesel & gasoil y electrical consume\n");
-  res.write("===============================\n");
-
-  res.write("Autores \n");
-  res.write("Francisco Javier Fernandez Rodriguez \n");
-  res.write("Gonzalo Romero Castillo \n");
-  res.write("Angel Suares Mora \n");
+    res.write("Hello world");
 
     res.end();
   });
+
+  app.get("/about", (req, res) => {
+
+      res.write("Tematica: co2, diesel & gasoil y electrical consume\n");
+      res.write("===============================\n");
+
+      res.write("Autores \n");
+      res.write("Francisco Javier Fernandez Rodriguez \n");
+      res.write("Gonzalo Romero Castillo \n");
+      res.write("Angel Suares Mora \n");
+
+      res.end();
+    });
 
   app.listen(process.env.PORT);
