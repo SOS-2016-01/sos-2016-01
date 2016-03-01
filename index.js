e52a3a250d20d23afb64025f9c02c81abe78a7b6
javyxz <javyxz@hotmail.com>
2016-03-01 17:44:39 +0100

    pasar a proyecto 2

---
+++ index.js
@@ -0,0 +1,62 @@
+var express = require("express");
+
+  var app = express();
+
+  app.get("/", (req, res) => {
+
+      res.write("Hello world");
+
+      res.end();
+    });
+
+    app.get("/about", (req, res) => {
+
+        res.write("thematic: co2, diesel & gasoil y electrical consume\n");
+        res.write("===============================\n");
+
+        res.write("authors: \n");
+        res.write("===============================\n");
+        res.write("Francisco Javier Fernandez Rodriguez \n");
+        res.write("Gonzalo Romero Castillo \n");
+        res.write("Angel Suarez Mora \n");
+
+        res.end();
+      });
+
+      app.get("/about/oil", (req, res) => {
+
+          res.write("thematic: oil\n");
+          res.write("===============================\n");
+
+          res.write("author: \n");
+          res.write("===============================\n");
+          res.write("Francisco Javier Fernandez Rodriguez \n");
+
+          res.end();
+        });
+
+        app.get("/about/co2", (req, res) => {
+
+            res.write("thematic: co2\n");
+            res.write("===============================\n");
+
+            res.write("author: \n");
+            res.write("===============================\n");
+            res.write("Gonzalo Romero Castillo\n");
+
+            res.end();
+          });
+
+          app.get("/about/electrical-consume", (req, res) => {
+
+              res.write("thematic: electrical consume\n");
+              res.write("===============================\n");
+
+              res.write("author: \n");
+              res.write("===============================\n");
+              res.write("Angel Suarez Mora \n");
+
+              res.end();
+            });
+
+    app.listen(process.env.PORT);
