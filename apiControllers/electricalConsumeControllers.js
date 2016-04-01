72698ceab03b80a0b0db1ec400caa59067e64d28
AngelSuaMor <angelsm95@hotmail.com>
2016-04-01 17:55:28 +0200

    big test878

--- apiControllers/electricalConsumeControllers.js
+++ apiControllers/electricalConsumeControllers.js
@@ -3,9 +3,8 @@ var key = "asmsfc";
 
 module.exports.getData = function(req,res){
 var apikey = req.query.apikey;
-//var limit = req.query.limit;
 if(apikey && apikey===key){
-  console.log("New GET of resource electrical consume");
+  console.log("New GET of resource electrical");
   res.send(JSON.stringify(data));
   }
 else{
