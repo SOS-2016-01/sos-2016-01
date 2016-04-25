cb1aaaebd67b0fcf4128b0fcff527238d00f7c61
javyxz <javyxz@hotmail.com>
2016-04-25 13:08:01 +0200

    closes #31

--- static/data/oil/functions.js
+++ static/data/oil/functions.js
@@ -28,3 +28,35 @@
     if(status=="error")
       console.log("Status: "+jqXHR.status);
   });
+
+  $(document).ready(function (data) {
+      // prepare the data
+      var url = "/api/v1/oil?apikey=12345";
+      var source =
+                {
+                    datatype: "json",
+                    datafields: [
+                          { name: 'country' },
+                          { name: 'year' },
+                          { name: 'diesel'},
+                          { name: 'gasoline' }
+                      ],
+                    id: 'id',
+                    url: url
+                };
+                var dataAdapter = new $.jqx.dataAdapter(source, {
+                    downloadComplete: function (data, status, xhr) { },
+                    loadComplete: function (data) { },
+                    loadError: function (xhr, status, error) { }
+      });
+      $("#jqxgrid").jqxGrid(
+      {
+          source: dataAdapter,
+          columns: [
+            { text: 'Country', datafield: 'country'},
+            { text: 'Year', datafield: 'year'},
+            { text: 'Diesel', datafield: 'diesel'},
+            { text: 'Gasoline', datafield: 'gasoline' }
+          ]
+      });
+  });
