f204e1c7e608654ccc9386670cf24ff037f69945
Gonzalo Romero Castillo <gonzy1235@hotmail.com>
2016-04-29 17:15:17 +0200

    New page data

--- static/data/co2/dataControllers.js
+++ static/data/co2/dataControllers.js
@@ -6,7 +6,7 @@ $("body").ready(function (){
     var apikey = $("#apikey").val();
     console.log("Apikey handled: "+apikey)
     var request = $.ajax({
-      url:"/api/v1/co2?apikey="+apikey,
+      url:"/api/v1/co2/loadInitialData?apikey="+apikey,
       type:"GET",
       contentType : "application/json"
     });
@@ -14,46 +14,14 @@ $("body").ready(function (){
     request.done(function (data,jqXHR,status){
       var list;
       console.log("Handling request (OK)");
-      console.log("Data received: ");
-      console.log(data);
-      $("#log").text("");
-      $("#data").text("");
-      $("#status").text("");
-      $("#list").text("");
-      $("#data").text(JSON.stringify(data));
-      dataToHtml(data);
       console.log("Status: "+jqXHR.status);
     });
 
     request.always(function (jqXHR,status){
-      $("#status").text(status);
       if(status=="error"){
-        $("#status").text(jqXHR.status + " " + jqXHR.statusText);
         console.log("Status: "+jqXHR.status);
-        $("#data").text("");
-        $("#list").text("");
       }
     });
-    function dataToHtml(data){
-      mylist = $('<ul></ul>')
-      if(data[0].co2kg){
-      for (i=0;i<data.length;i++){
-          $('<li></li>').text("Country="+data[i].country+", Year="+data[i].year+", co2mtn="+data[i].co2mtn+", co2kg="+data[i].co2kg).appendTo(mylist);
-        }
-      }
-      else if(data[0].diesel){
-        for (i=0;i<data.length;i++){
-        $('<li></li>').text("Country="+data[i].country+", Year="+data[i].year+", diesel="+data[i].diesel+", gasoline="+data[i].gasoline).appendTo(mylist);
-        }
-      }
-      else if(data[0].ePowerConsum){
-        for (i=0;i<data.length;i++){
-        $('<li></li>').text("Country="+data[i].country+", Year="+data[i].year+", ePowerConsum="+data[i].ePowerConsum+", energyUse="+data[i].energyUse+", urbanPopulation="+data[i].urbanPopulation).appendTo(mylist);
-        }
-      }
-      console.log("List:"+mylist.html());
-      mylist.appendTo("#list");
-    }
   });
 
   $("#Delete_all").click(function(){
@@ -82,165 +50,87 @@ $("body").ready(function (){
   });
 
 
+
 });
 
 function loadTable(){
-      $('#tt').datagrid({
-        title:'Co2 DataGrid',
-        //iconCls:'icon-edit',
-        width:450,
-        cache:false,
-        singleSelect:true,
-        pagination:true,
-        //idField:'itemid',
-        url:'/api/v1/co2?apikey=vdcgrc',
-        method: 'GET',
-        columns:[[
-          {field:'country',title:'Country',width:86,align:'center',editor:'text'},
-          {field:'year',title:'Year',width:80,align:'center',editor:'numberbox'},
-          {field:'co2mtn',title:'CO2 MTN',width:80,align:'center',editor:'numberbox'},
-          {field:'co2kg',title:'CO2 KG',width:80,align:'center',editor:'numberbox'},
-          {field:'action',title:'Action',width:90,align:'center',
-            formatter:function(value,row,index){
-              if (row.editing){
-                var s = '<a href="#" >Save</a> ';
-                var c = '<a href="#" >Cancel</a>';
-                return s+c;
-              } else {
-                var e = '<a href="#" ><span class="icon-pencil">Editar</span></a> ';
-                var d = '<a href="#" onclick="deleterow(this)">Eliminar<span class="icon-cross"></span></a>';
-                return e+d;
-              }
-            }
-          }
-        ]],
-
-        onLoadSuccess:function(data) {
-            var tr = $("tbody")[3];
-            var hijos = ($(tr).children());
-            for(var i = 0; i < hijos.length; i++){
-              var country = $(hijos[i]).children()[0];
-              country = $(country).first().text();
-              var year = $(hijos[i]).children()[1];
-              year = $(year).first().text();
-              var temp3 = $(hijos[i]).children()[3];
-              var divDeEnlaces = $(temp3).first().children()[0];
-              var enlaceEditar = $(divDeEnlaces).children()[0];
-              console.log(enlaceEditar);
-              var enlaceBorrar = $(divDeEnlaces).children()[1];
-              console.log(enlaceBorrar);
-              $(enlaceEditar).attr('href', 'editResourceCo.html?country='+country+'&year='+year);
-              //$(enlaceBorrar).attr('href', 'deleteResource.html?country='+country+'&year='+year);
-            }
-        },
-         onBeforeEdit:function(index,row){
-            row.editing = true;
-            updateActions(index);
-         },
-        onAfterEdit:function(index,row){
-            row.editing = false;
-            updateActions(index);
-        },
-        onCancelEdit:function(index,row){
-            row.editing = false;
-            updateActions(index);
-        }
-      });
-    };
-    function updateActions(index){
-      $('#tt').datagrid('updateRow',{
-        index: index,
-        row:{}
-      });
+  console.log("Handling load table");
+  var apikey = $("#apikey").val();
+  var items = $("#items").val();
+  var page = $("#page").val();
+  var request = $.ajax({
+    url:"/api/v1/co2?apikey="+apikey+"&limit="+items+"&offset="+items*page,
+    type:"GET",
+    contentType : "application/json"
+  });
+  request.done(function (data){
+    console.log("Handling request (OK)");
+    console.log("Data received: ");
+    console.log(JSON.stringify(data));
+    items = $("#items").val();
+    page = $("#page").val();
+  //  array = JSON.parse(data);
+    mytable = $('<tbody></tbody>')
+    for (i=0;i<data.length;i++){
+      var row = $('<tr></tr>').appendTo(mytable);
+      $('<td></td>').text(data[i].country).appendTo(row);
+      $('<td></td>').text(data[i].year).appendTo(row);
+      $('<td></td>').text(data[i].co2mtn).appendTo(row);
+      $('<td></td>').text(data[i].co2kg).appendTo(row);
+      $('<td></td>').html('<i class="small material-icons">mode_edit</i><i class="small material-icons">delete</i>').appendTo(row);
     }
-    function getRowIndex(target){
-      var tr = $(target).closest('tr.datagrid-row');
-      var trcolum = $(tr).first();
-      console.log(trcolum);
-      return parseInt(tr.attr('datagrid-row-index'));
+    console.log("TTTTT:"+mytable.html());
+    mytable.appendTo("#tt");
+  });
+
+  request.always(function (jqXHR,status){
+    if(status=="error"){
+      console.log("Status: "+jqXHR.status);
+      if(jqXHR.status==401)
+      alert("You entered a wrong APIkey")
     }
-    function editrow(target){
-      $('#tt').datagrid('beginEdit', getRowIndex(target));
-      console.log(getRowIndex(target));
+
+  });
+}
+
+function searchTable(){
+  console.log("Handling search table");
+  var apikey = $("#apikey").val();
+  var fromS = $("#from").val();
+  var to = $("#to").val();
+  if(fromS&&to){
+  var request = $.ajax({
+    url:"/api/v1/co2?apikey="+apikey+"&from="+fromS+"&to="+to,
+    type:"GET",
+    contentType : "application/json"
+  });
+  request.done(function (data){
+    console.log("Handling request (OK)");
+    console.log("Data received: ");
+    console.log(JSON.stringify(data));
+    items = $("#items").val();
+    page = $("#page").val();
+  //  array = JSON.parse(data);
+    mytable = $('<tbody></tbody>')
+    for (i=0;i<data.length;i++){
+      var row = $('<tr></tr>').appendTo(mytable);
+      $('<td></td>').text(data[i].country).appendTo(row);
+      $('<td></td>').text(data[i].year).appendTo(row);
+      $('<td></td>').text(data[i].co2mtn).appendTo(row);
+      $('<td></td>').text(data[i].co2kg).appendTo(row);
+      $('<td></td>').html('<i class="small material-icons">mode_edit</i><i class="small material-icons">delete</i>').appendTo(row);
     }
-    function deleterow(target){
+    console.log("TTTTT:"+mytable.html());
+    mytable.appendTo("#tt");
+  });
 
-      // Una vez terminada la request, se borra de la tabla.
-      var win= $.messager.confirm('Confirm','Are you sure?',function(r){
-        if (r){
-          // Borrado de la BD
-
-        // Obtenemos Country y Year
-        var tr = $("tbody")[3];
-        var hijos = ($(tr).children());
-        var country, year;
-        var indice = getRowIndex(target);
-        console.log(indice);
-        country = $(hijos[indice]).children()[0];
-        country = $(country).first().text();
-        year = $(hijos[indice]).children()[1];
-        year = $(year).first().text();
-
-        console.log(country + " " + year);
-        // Petición de Delete a la BD.
-        var request = $.ajax({
-          url: '/api/v1/co2/'+country+'/'+year+"?apikey=vdcgrc",
-          type: 'DELETE',
-        });
-
-        request.done(function(data, status, jqXHR){
-          if(data != ""){
-            //data = '[{"country": "spain"}, {"country": "portugal"}]';
-            $("#data").text(data);
-            var parsedData = $.parseJSON(data);
-             $("#list").empty();
-
-             if(Array.isArray(parsedData)){
-               for(var i in parsedData){
-
-                 var dump = "";
-                 var obj = parsedData[i];
-                 for(var prop in obj){
-                   dump += "[" + prop + " = " + obj[prop] + "]";
-                 }
-                 $("#list").append("<li>"+dump+"</li>");
-               }
-             }else{
-              var dump = "";
-              var obj = parsedData;
-              for(var prop in obj){
-                dump += "[" + prop + " = " + obj[prop] + "]";
-              }
-             }
-          }else{
-            $("#list").empty();
-            $("#data").empty();
-          }
-        });
-
-        request.always(function(jqXHR, status){
-          if(status == "error"){
-            $("#status").text(jqXHR.status);
-            $("#list").empty();
-            $("#data").empty();
-          }else{
-            $("#status").text("200 OK");
-            console.log("Done!");
-          }
-          $("#log").text("Done! ");
-        });
-
-            $('#tt').datagrid('deleteRow', getRowIndex(target));
-          }
-        });
-        win.window('move',{
-          top:100
-          });
-
+  request.always(function (jqXHR,status){
+    if(status=="error"){
+      console.log("Status: "+jqXHR.status);
+      if(jqXHR.status==401)
+      alert("You entered a wrong APIkey")
     }
-    function saverow(target){
-      $('#tt').datagrid('endEdit', getRowIndex(target));
-    }
-    function cancelrow(target){
-      $('#tt').datagrid('cancelEdit', getRowIndex(target));
-    }
+  });
+  }
+  else alert("Insert a valid intervale");
+}
