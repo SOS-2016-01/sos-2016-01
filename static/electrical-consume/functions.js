f191bc7703669650f9ab5777abe1b113c79ae0a3
AngelSuaMor <angelsm95@hotmail.com>
2016-04-30 18:23:14 +0200

    no final version

--- static/electrical-consume/functions.js
+++ static/electrical-consume/functions.js
@@ -1,48 +1,214 @@
-$("body").ready(function(){
-  console.log("Jquery Ready!");
+$("body").ready(function (){
+  console.log("jQuery Ready!");
 
 
   $("#loadInitialData").click(function(){
-      console.log("Entra");
-      var apikey = $("#apikey").val();
-      var dir = "";
+        console.log("Handling click on load data");
 
-      var request = $.ajax({
-        url:"/api/v1/electrical-consume/loadInitialData?apikey="+apikey,
-        type:"GET"
-      });
+        var apikey = $("#apikey").val();
+        console.log("Apikey handled: "+apikey)
+        var request = $.ajax({
+            url:"/api/v1/electrical-consume/loadInitialData?apikey="+apikey,
+            type:"GET",
+            contentType : "application/json"
+        });
 
-      var request = $.ajax({
-        url:"/api/v1/electrical-consume?apikey="+apikey,
-        type:"GET"
-      });
+        request.done(function (data,jqXHR,status){
+            $("#t1").find("tr:gt(0)").remove();
+            table("");
+            console.log("Handling request (OK)");
+            console.log("Status: "+jqXHR.status);
+        });
 
-      request.done(function (data){
-        console.log("Handling request (OK)");
-        console.log("Data received: ");
-        showTable(data);
+        request.always(function (jqXHR,status){
+            if(status=="error"){
+              console.log("Status: "+jqXHR.status);
+            }
+        });
+  });
 
-      });
+  $("#deleteAll").click(function(){
+        var apikey = $("#apikey").val();
+
+        var request = $.ajax({
+            url:"/api/v1/electrical-consume?apikey="+apikey,
+            type:"DELETE"
+        });
+
+        request.done(function (data){
+            console.log("Handling request (OK)");
+            $("#t1").find("tr:gt(0)").remove();
+            table("");
+        });
+
+        request.always(function (){
+            if(status=="error"){
+              console.log("Status: "+jqXHR.status);
+            }
+        });
+    });
+
+    $("#deleteItem").click(function(){
+        var apikey = $("#apikey").val();
+        var country = $("#country").val();
+        var year = $("#year").val();
+        if(country && year){
+
+        var request = $.ajax({
+            url:"/api/v1/electrical-consume/"+country+"/"+year+"?apikey="+apikey,
+            type:"DELETE"
+        });
+
+        request.done(function (data){
+            console.log("Handling request (OK)");
+            $("#t1").find("tr:gt(0)").remove();
+            table("");
+
+        });
+
+        request.always(function (jqXHR,status){
+            if(status=="error"){
+              console.log("Status: "+jqXHR.status);
+            }
+        });
+      }//poner emergente
+    });
 
 
+    $("#search").click(function(){
+            console.log("Handling click");
+            var apikey = $("#apikey").val();
+            var country = $("#country").val();
+            var year = $("#year").val();
+            var url="";
+
+            if(country && year){
+              url = "/api/v1/electrical-consume/"+country+"/"+year+"?apikey="+apikey;
+            }else if(year){
+              url = "/api/v1/electrical-consume/"+year+"?apikey="+apikey;
+            }else if (country){
+              url = "/api/v1/electrical-consume/"+country+"?apikey="+apikey;
+            }
+
+            var request = $.ajax({
+              url:"/api/v1/electrical-consume?apikey="+apikey,
+              type:"GET"
+            });
+
+            request.done(function (data){
+              console.log("Handling request (OK)");
+              $("#t1").find("tr:gt(0)").remove();
+              table(url);
+            });
+
+            request.always(function (jqXHR,status){
+              if(status=="error"){
+                console.log("Status: "+jqXHR.status);
+              }
+            });
+          });
+
+
+      $("#create").click(function(){
+            var apikey = $("#apikey").val();
+            var country = $("#country").val();
+            var year = $("#year").val();
+            var ePowerConsum = $("#ePowerConsum").val();
+            var energyUse = $("#energyUse").val();
+            var urbanPopulation = $("#urbanPopulation").val();
+            var url = "";
+
+           if(country && year && ePowerConsum && energyUse && urbanPopulation){
+
+            var request = $.ajax({
+                url:"/api/v1/electrical-consume?apikey="+apikey,
+                type:"POST",
+                data: '{"country":"'+country+'","year":'+year+',"ePowerConsum":'+ePowerConsum+',"energyUse":'+energyUse+',"urbanPopulation":'+urbanPopulation+'}',
+                contentType : "application/json"
+            });
+
+            request.done(function (data){
+                console.log("Handling request (OK)");
+                $("#t1").find("tr:gt(0)").remove();
+                table("");
+            });
+
+            request.always(function (jqXHR,status){
+                if(status=="error"){
+                  console.log("Status: "+jqXHR.status);
+                }
+            });
+            }//emergente
+    });
+
+    $("#update").click(function(){
+          var apikey = $("#apikey").val();
+          var country = $("#country").val();
+          var year = $("#year").val();
+          var ePowerConsum = $("#ePowerConsum").val();
+          var energyUse = $("#energyUse").val();
+          var urbanPopulation = $("#urbanPopulation").val();
+          var url = "";
+
+          if(country && year && ePowerConsum && energyUse && urbanPopulation){
+          var request = $.ajax({
+            url:"/api/v1/electrical-consume/"+country+"/"+year+"?apikey="+apikey,
+            type:"PUT",
+            data: '{"country":"'+country+'","year":'+year+',"ePowerConsum":'+ePowerConsum+',"energyUse":'+energyUse+',"urbanPopulation":'+urbanPopulation+'}',
+            contentType : "application/json"
+          });
+
+          request.done(function (data){
+            console.log("Handling request (OK)");
+            $("#t1").find("tr:gt(0)").remove();
+            table("");
+          });
+
+          request.always(function (jqXHR,status){
+            if(status=="error"){
+              console.log("Status: "+jqXHR.status);
+            }
+          });
+        }//emergente
     });
 
 
 
-});
 
-function showTable(data){
+  });
 
-  console.log("showTable"+data[0]);
-  var mytable = $('<tbody></tbody>');
-  for (i=0;i<data.length;i++){
-    var row = $('<tr></tr>').appendTo(mytable);
-    $('<td></td>').text(data[i].country).appendTo(row);
-    $('<td></td>').text(data[i].year).appendTo(row);
-    $('<td></td>').text(data[i].ePowerConsum).appendTo(row);
-    $('<td></td>').text(data[i].energyUse).appendTo(row);
-    $('<td></td>').text(data[i].urbanPopulation).appendTo(row);
+
+
+function table(url){
+  console.log("Handling load table");
+  console.log(url);
+  $("#t1").find("tr:gt(0)").remove();
+  var apikey = $("#apikey").val();
+  items = $("#items").val();
+  page = $("#page").val();
+
+  if(url==""){
+    url="/api/v1/electrical-consume?apikey="+apikey+"&limit="+items+"&offset="+items*(page-1);
+    console.log(url);
   }
-  console.log("TTTTT:"+mytable.html());
-  mytable.appendTo("#t1");
+
+  var request = $.ajax({
+    url:url,
+    type:"GET",
+    contentType : "application/json"
+  });
+  request.done(function (data){
+    console.log("Handling request (OK)");
+    mytable = $('<tbody></tbody>')
+    for (i=0;i<data.length;i++){
+      var row = $('<tr></tr>').appendTo(mytable);
+      $('<td></td>').text(data[i].country).appendTo(row);
+      $('<td></td>').text(data[i].year).appendTo(row);
+      $('<td></td>').text(data[i].ePowerConsum).appendTo(row);
+      $('<td></td>').text(data[i].energyUse).appendTo(row);
+      $('<td></td>').text(data[i].urbanPopulation).appendTo(row);
+    }
+    console.log("TTTTT:"+mytable.html());
+    mytable.appendTo("#t1");
+  });
 }
