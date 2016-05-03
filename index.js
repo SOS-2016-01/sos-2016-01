3442ca31165d640778ae9ed468dc11991a811d81
javyxz <javyxz@hotmail.com>
2016-05-03 11:44:22 +0200

    closes #38

--- index.js
+++ index.js
@@ -22,9 +22,12 @@ var oilControllers = require('./apiControllers/oilControllers.js');
   app.use("/electrical-consume",express.static(__dirname + '/static/electrical-consume'));
 
   app.use("/oil",express.static(__dirname + '/static/oil'));
+
+  app.use("/co2",express.static(__dirname + '/static/co2'));
+
   app.use("/",express.static(__dirname + '/static'));
 
-    app.use("/co2",express.static(__dirname + '/static/co2'));
+
 
 
   // app.get("/", (req, res) => {
