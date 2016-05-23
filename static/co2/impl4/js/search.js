4a7342d72aaf5ff77f6c03476981b0a21aeae531
Gonzalo Romero Castillo <gonzy1235@hotmail.com>
2016-05-23 18:59:59 +0200

    Youtube fixied

--- static/co2/impl4/js/search.js
+++ static/co2/impl4/js/search.js
@@ -13,7 +13,7 @@ function search() {
   });
 
   request.execute(function(response) {
-    $("#search-container").remove();
+    $("#videos").remove();
     recived= response.result;
     var length =  recived.items.length;
     console.log(recived);
@@ -22,7 +22,7 @@ function search() {
     mycontainer = $('#search-container');
     //recived.items[0]
     for(i=0;i<length;i++){
-      $('<div class="row"><div class="col s12 m7"><div class="card"><div class="card-image"><img src='+recived.items[i].snippet.thumbnails.medium.url+'></div><div class="card-content"><p>'+recived.items[i].snippet.title+'</p></div><div class="card-action"><a href="https://www.youtube.com/watch?v="'+recived.items[i].id.videoId+'>Link</a></div></div></div></div>').appendTo(mycontainer);
+      $('<div class="row" id="videos"><div class="col s12 m7"><div class="card"><div class="card-image"><img src='+recived.items[i].snippet.thumbnails.medium.url+'></div><div class="card-content"><p>'+recived.items[i].snippet.title+'</p></div><div class="card-action"><a href="https://www.youtube.com/watch?v="'+recived.items[i].id.videoId+'>Link</a></div></div></div></div>').appendTo(mycontainer);
     }
   //$('#search-container').html('<pre>' + str + '</pre>');
   });
