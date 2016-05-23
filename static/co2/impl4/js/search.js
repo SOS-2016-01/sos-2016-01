13a2922013e462dcd7aa0be7a4e2f16f43609928
Gonzalo Romero Castillo <gonzy1235@hotmail.com>
2016-05-23 14:05:45 +0200

    visual changes impl4

--- static/co2/impl4/js/search.js
+++ static/co2/impl4/js/search.js
@@ -13,13 +13,13 @@ function search() {
   });
 
   request.execute(function(response) {
-    var str = JSON.stringify(response.result);
-    console.log(response.result);
     recived= response.result;
+    var str = JSON.stringify(recived);
+    console.log(recived);  
     mycontainer = $('#search-container');
     $('<div class="row"><div class="col s12 m7"><div class="card"><div class="card-image"><img src='+recived.items[0].snippet.thumbnails.medium.url+'></div><div class="card-content"><p>'+recived.items[0].snippet.title+'</p></div><div class="card-action"><a href=>Link</a></div></div></div></div>');
 //recived.items[0]
-    for(i=0;i<reponse.result.items.length;i++){
+    for(i=0;i<recived.items.length;i++){
     }
     //$('#search-container').html('<pre>' + str + '</pre>');
   });
