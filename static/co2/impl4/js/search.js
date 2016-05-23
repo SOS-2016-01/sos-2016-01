d1449da4782e59d5c996ea3c453bee183de9e2ca
Gonzalo Romero Castillo <gonzy1235@hotmail.com>
2016-05-23 13:56:09 +0200

    visual changes impl4

--- static/co2/impl4/js/search.js
+++ static/co2/impl4/js/search.js
@@ -15,6 +15,11 @@ function search() {
   request.execute(function(response) {
     var str = JSON.stringify(response.result);
     console.log(response.result);
-    $('#search-container').html('<pre>' + str + '</pre>');
+
+    mycontainer = $('#search-container');
+    $('<div class="row"><div class="col s12 m7"><div class="card"><div class="card-image"><img src='+reponse.result.items.snippet.thumbnails.medium.url+'></div><div class="card-content"><p>'+reponse.result.items.snippet.title+'</p></div><div class="card-action"><a href='+reponse.result.items+'>Link</a></div></div></div></div>')
+    for(i=0;i<reponse.result.items.length;i++){
+    }
+    //$('#search-container').html('<pre>' + str + '</pre>');
   });
 }
