3adf17030f778cd0d7ac32e6d2e6428ee7e84e62
Gonzalo Romero Castillo <gonzy1235@hotmail.com>
2016-05-23 18:24:48 +0200

    Input page fixed

--- static/co2/dataControllers.js
+++ static/co2/dataControllers.js
@@ -13,7 +13,7 @@ function charge_pages(){
     var pages = data.length/$("#items").val();
     $(".pagesValues").remove();
 
-    var select=$('#page');
+    var select=$('#inputPages');
 
     for(i=1;i<pages+1;i++){
       $('<option class="pagesValues" value='+i+'></option>').text(i).appendTo(select);
