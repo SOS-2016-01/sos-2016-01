0158d888d1ad4b19e43a48bba8757618d221faec
Gonzalo Romero Castillo <gonzy1235@hotmail.com>
2016-05-23 15:41:18 +0200

    visual changes impl4

--- static/co2/impl4/js/search.js
+++ static/co2/impl4/js/search.js
@@ -17,10 +17,10 @@ function search() {
     var str = JSON.stringify(recived);
     console.log(recived);
     mycontainer = $('#search-container');
-    $('<div class="row"><div class="col s12 m7"><div class="card"><div class="card-image"><img src='+recived.items[0].snippet.thumbnails.medium.url+'></div><div class="card-content"><p>'+recived.items[0].snippet.title+'</p></div><div class="card-action"><a href=>Link</a></div></div></div></div>').appendTo(mycontainer);
-//recived.items[0]
+    //recived.items[0]
     for(i=0;i<recived.items.length;i++){
     }
+    $('<div class="row"><div class="col s12 m7"><div class="card"><div class="card-image"><img src='+recived.items[i].snippet.thumbnails.medium.url+'></div><div class="card-content"><p>'+recived.items[i].snippet.title+'</p></div><div class="card-action"><a href="https://www.youtube.com/watch?v="'+recived.items[i].id.videoId+'>Link</a></div></div></div></div>').appendTo(mycontainer);
     //$('#search-container').html('<pre>' + str + '</pre>');
   });
 }
