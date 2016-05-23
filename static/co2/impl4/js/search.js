2143e11fd766eb8285b73157373a9d5ef3b6a5d9
Gonzalo Romero Castillo <gonzy1235@hotmail.com>
2016-05-23 15:49:38 +0200

    visual changes impl4

--- static/co2/impl4/js/search.js
+++ static/co2/impl4/js/search.js
@@ -21,8 +21,8 @@ function search() {
     mycontainer = $('#search-container');
     //recived.items[0]
     for(i=0;i<length;i++){
+      $('<div class="row"><div class="col s12 m7"><div class="card"><div class="card-image"><img src='+recived.items[i].snippet.thumbnails.medium.url+'></div><div class="card-content"><p>'+recived.items[i].snippet.title+'</p></div><div class="card-action"><a href="https://www.youtube.com/watch?v="'+recived.items[i].id.videoId+'>Link</a></div></div></div></div>').appendTo(mycontainer);
     }
-    $('<div class="row"><div class="col s12 m7"><div class="card"><div class="card-image"><img src='+recived.items[i].snippet.thumbnails.medium.url+'></div><div class="card-content"><p>'+recived.items[i].snippet.title+'</p></div><div class="card-action"><a href="https://www.youtube.com/watch?v="'+recived.items[i].id.videoId+'>Link</a></div></div></div></div>').appendTo(mycontainer);
-    //$('#search-container').html('<pre>' + str + '</pre>');
+  //$('#search-container').html('<pre>' + str + '</pre>');
   });
 }
