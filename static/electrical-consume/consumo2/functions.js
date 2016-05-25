791ab241338383c145ceecb5c1e30086d625a179
javyxz <javyxz@hotmail.com>
2016-05-25 20:23:09 +0200

    angelito

--- static/electrical-consume/consumo2/functions.js
+++ static/electrical-consume/consumo2/functions.js
@@ -1,41 +1,43 @@
-
-  console.log("Handling request");
-
-  var request = $.ajax({
-    url:"https://www.googleapis.com/books/v1/volumes?q=harry+potter&callback=handleResponse",
-    type:"GET"
-  });
-  request.done(function (data){
-    console.log("Handling request (OK)");
-    console.log("Data received: ");
-    console.log(JSON.stringify(data));
-    //array = JSON.parse(data);
-    mytable = $('<tbody></tbody>')
-    for (i=0;i<data.length;i++){
-      var row = $('<tr></tr>').appendTo(mytable);
-      $('<td></td>').text(data[i].title).appendTo(row);
-      $('<td></td>').text(data[i].listPrice.amount).appendTo(row);
-    }
-    console.log("TTTTT:"+mytable.html());
-    mytable.appendTo("#t1");
-
-  });
-
-  request.always(function (jqXHR,status){
-    if(status=="error")
-      console.log("Status: "+jqXHR.status);
-  });
+  // 
+  // console.log("Handling request");
+  //
+  // var request = $.ajax({
+  //   url:"https://www.googleapis.com/books/v1/volumes?q=harry+potter&callback=handleResponse",
+  //   type:"GET"
+  // });
+  // request.done(function (data){
+  //   console.log("Handling request (OK)");
+  //   console.log("Data received: ");
+  //   console.log(JSON.stringify(data));
+  //   //array = JSON.parse(data);
+  //   mytable = $('<tbody></tbody>')
+  //   for (i=0;i<data.length;i++){
+  //     var row = $('<tr></tr>').appendTo(mytable);
+  //     $('<td></td>').text(data[i].title).appendTo(row);
+  //     $('<td></td>').text(data[i].listPrice.amount).appendTo(row);
+  //   }
+  //   console.log("TTTTT:"+mytable.html());
+  //   mytable.appendTo("#t1");
+  //
+  // });
+  //
+  // request.always(function (jqXHR,status){
+  //   if(status=="error")
+  //     console.log("Status: "+jqXHR.status);
+  // });
 
   $(document).ready(function () {
       // prepare the data
-      var url = "http://sos-2016-01/find?v=1.0&q=Official%20Google%20Blogs";
+      var url = "/ajax/services/feed/find?v=1.0&q=Official%20Google%20Blogs";
 
       var source =
                 {
                     datatype: "json",
                     datafields: [
-                          { name: 'Titulo' },
-                          { name: 'Price' }
+                          { name: 'url' },
+                          { name: 'title' },
+                          { name: 'contentSnippet'},
+                          { name: 'link' }
                       ],
                     id: 'id',
                     url: url
@@ -49,8 +51,10 @@
       {
           source: dataAdapter,
           columns: [
-            { text: 'Titulo', datafield: 'title'},
-            { text: 'Price', datafield: 'amount'}
+            { text: 'url', datafield: 'url'},
+            { text: 'title', datafield: 'title'},
+            { text: 'contentSnippet', datafield: 'contentSnippet'},
+            { text: 'link', datafield: 'link'}
           ]
       });
   });
